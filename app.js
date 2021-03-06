3efc31f4fa53e850b2b0b67629e7fa93781157f7
Long <chenlong05188@gmail.com>
2021-03-05 21:11:38 -0500

    first commit Todo List app

---
+++ app.js
@@ -0,0 +1,61 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
+// Functions
+function addTodo(event){
+    // Prevent form from submitting
+    event.preventDefault();
+    console.log("Hello");
+    // Todo Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");  // equal to : todoDiv.className="todo";
+    // Create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    // Append LI on Div
+    todoDiv.appendChild(newTodo);
+    // Complete button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-button");
+    todoDiv.appendChild(completedButton);
+    // Delete button
+    const DeleteButton = document.createElement("button");
+    DeleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    DeleteButton.classList.add("trash-button");
+    todoDiv.appendChild(DeleteButton);
+
+    // Append the whole Div to unordered list
+    todoList.appendChild(todoDiv);
+
+    // Set Input Value to empty
+    todoInput.value="";
+}
+
+function deleteCheck(event){
+    const item = event.target; // target is what we are clicking on 
+    // Delete Todo
+    if (item.classList[0] === 'trash-button'){
+        const todo = item.parentElement;
+        // Add fall animation
+        todo.classList.add("fall");
+        //todo.remove();
+        // function executed after trasnsition end
+        todo.addEventListener("transitioned",function(){
+            todo.remove();
+        });
+    }
+    // Check Todo
+    const todo = item.parentElement;
+    todo.classList.toggle("completed"); // The classList.toggle("", boolean) method supports adding and removing CSS classes 
+    
+
+
+}
